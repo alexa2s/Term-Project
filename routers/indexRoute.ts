325683121c9f1e7926d2d4d6d8535fcd0f80753c
Anthony Cao <anthonycao560@gmail.com>
2026-04-11 16:56:43 -0700

    test

--- routers/indexRoute.ts
+++ routers/indexRoute.ts
@@ -1,12 +1,5 @@
 import express, { Request, Response } from "express";
-import { getSubs } from "../fake-db";
 const router = express.Router();
-router.get("/list", (req,res) => {
-  const subs = getSubs()
-  subs.sort((a, b) => a.localeCompare(b));
-    
-    res.render("subsList", { subs });
-})
 
 router.get("/", async (req: Request, res: Response) => {
   res.redirect("/posts");
--- routers/subsRouters.ts
+++ routers/subsRouters.ts
@@ -1,16 +1,39 @@
 // const { ensureAuthenticated } = require("../middleware/checkAuth");
 import express from "express";
 import * as database from "../controller/postController";
+import { getSubs, getPosts, decoratePost } from "../fake-db";
 const router = express.Router();
 
 router.get("/list", async (req, res) => {
   // ⭐ TODO
-  res.render("subs");
+  const sublist = getSubs()
+  
+  sublist.sort((a, b) => a.localeCompare(b));
+  // res.render("subs");
+  res.render("subs", {subs: sublist});
 });
 
 router.get("/show/:subname", async (req, res) => {
-  // ⭐ TODO
-  res.render("sub");
+    // ⭐ TODO
+  const subname = req.params.subname as string;
+  
+  // 1. Get posts and decorate them (so usernames show up)
+  const rawPosts = getPosts(20, subname);
+  const decoratedPosts = rawPosts.map(post => decoratePost(post));
+
+  // 2. Render "sub" (This matches sub.ejs)
+  res.render("sub", { 
+    name: subname, 
+    subpost: decoratedPosts 
+  });
 });
 
 export default router;
+  // // function getPosts(n = 5, sub = undefined) {
+  //   let allPosts = Object.values(posts);
+  //   if (sub) {
+  //     allPosts = allPosts.filter((post) => post.subgroup === sub);
+  //   }
+  //   allPosts.sort((a, b) => b.timestamp - a.timestamp);
+  //   return allPosts.slice(0, n);
+  // }
