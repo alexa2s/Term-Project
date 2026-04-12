b60ce61f06f67ec73c9f2815d23ec795ac26e86e
Anthony Cao <anthonycao560@gmail.com>
2026-04-11 23:56:16 -0700

    test

--- routers/postRouters.ts
+++ routers/postRouters.ts
@@ -3,7 +3,14 @@ import express from "express";
 import * as database from "../controller/postController";
 const router = express.Router();
 import { ensureAuthenticated } from "../middleware/checkAuth";
-
+import {
+  getUser,
+  addPost,
+  getPost,
+  decoratePost,
+  editPost,
+  deletePost,
+} from "../fake-db";
 router.get("/", async (req, res) => {
   const posts = await database.getPosts(20);
   const user = await req.user;
@@ -16,27 +23,62 @@ router.get("/create", ensureAuthenticated, (req, res) => {
 
 router.post("/create", ensureAuthenticated, async (req, res) => {
   // ⭐ TODO
+  const user = await req.user;
+  const title = req.body.title;
+  const link = req.body.link;
+  const description = req.body.description;
+  const creator = user.id;
+
+  const subgroup = req.body.subgroup;
+  if (!title) {
+    return res.status(400).send("Post needs a Title");
+  }
+
+  const addp = addPost(title, link, creator, description, subgroup);
+
+  res.redirect(`/posts/show/${addp.id}`);
 });
 
 router.get("/show/:postid", async (req, res) => {
   // ⭐ TODO
-  res.render("individualPost");
+  const postid = req.params.postid;
+  const poststuff = getPost(postid);
+  const user = await req.user;
+
+  res.render("individualPost", { post: poststuff, user: user });
 });
 
 router.get("/edit/:postid", ensureAuthenticated, async (req, res) => {
   // ⭐ TODO
+  const user = await req.user;
+  const postid = req.params.postid;
+  const poststuff = getPost(postid);
 });
 
 router.post("/edit/:postid", ensureAuthenticated, async (req, res) => {
   // ⭐ TODO
+  postid = req.params.postid;
+
+  res.redirect(`/posts/show/${postid}`);
 });
 
 router.get("/deleteconfirm/:postid", ensureAuthenticated, async (req, res) => {
   // ⭐ TODO
+  const user = await req.user;
+  const postid = req.params.postid;
+
+  const poststuff = getPost(postid);
+  res.render("indi")
 });
 
 router.post("/delete/:postid", ensureAuthenticated, async (req, res) => {
   // ⭐ TODO
+
+  postid = req.params.postid;
+
+  deletePost(postid);
+  res.redirect();
+  
 });
 
 router.post(
@@ -44,7 +86,7 @@ router.post(
   ensureAuthenticated,
   async (req, res) => {
     // ⭐ TODO
-  }
+  },
 );
 
 export default router;
--- routers/subsRouters.ts
+++ routers/subsRouters.ts
@@ -17,23 +17,11 @@ router.get("/show/:subname", async (req, res) => {
     // ⭐ TODO
   const subname = req.params.subname as string;
   
-  // 1. Get posts and decorate them (so usernames show up)
-  const rawPosts = getPosts(20, subname);
-  const decoratedPosts = rawPosts.map(post => decoratePost(post));
+  
+  const subposts  = getPosts(20, subname);
+  const decoratedPosts = subposts.map(post => decoratePost(post));
 
-  // 2. Render "sub" (This matches sub.ejs)
-  res.render("sub", { 
-    name: subname, 
-    subpost: decoratedPosts 
-  });
-});
+ 
+  res.render("sub", {  name: subname, subpost: decoratedPosts  })});
 
 export default router;
-  // // function getPosts(n = 5, sub = undefined) {
-  //   let allPosts = Object.values(posts);
-  //   if (sub) {
-  //     allPosts = allPosts.filter((post) => post.subgroup === sub);
-  //   }
-  //   allPosts.sort((a, b) => b.timestamp - a.timestamp);
-  //   return allPosts.slice(0, n);
-  // }
